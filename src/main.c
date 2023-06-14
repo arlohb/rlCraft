7a8940d1a37fc75d83b8db8e456fe0b6efa121de
arlohb <arlo@urwick.co.uk>
2023-06-14 08:21:11 +0100

    Added basic 3d

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <raylib.h>
+#include <raymath.h>
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -6,9 +8,30 @@
 int main() {
     InitWindow(WIDTH, HEIGHT, "RLCraft");
 
+    Camera3D camera = {};
+    camera.position = (Vector3) { 0, 10, 10 };
+    camera.target = Vector3Zero();
+    camera.up = (Vector3) { 0, 1, 0 };
+    camera.fovy = 45;
+    camera.projection = CAMERA_PERSPECTIVE;
+    SetCameraMode(camera, CAMERA_FREE);
+
+    SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
+
             ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawCube(Vector3Zero(), 2, 2, 2, RED);
+
+                DrawGrid(10, 1);
+
+                UpdateCamera(&camera);
+
+            EndMode3D();
 
         EndDrawing();
     }
